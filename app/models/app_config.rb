01aa618ff6da2d99399c6d5d34a20f1e5b1819a5
jimmy <solo123@21cn.com>
2016-11-01 20:53:10 +0800

    app_config

--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -1,19 +1,19 @@
 class AppConfig < ApplicationRecord
-  scope :show_order, -> {order(:group)}
+  scope :show_order, -> {order(:group, :name)}
 
-  def self.get(key)
-    if r = AppConfig.find_by(group: key)
+  def self.get(group, name)
+    if r = AppConfig.find_by(group: group, name: name)
       r.val
     else
       nil
     end
   end
 
-  def self.set(key, val)
-    if r = AppConfig.find_by(group: key)
+  def self.set(group, name, val)
+    if r = AppConfig.find_by(group: group, name: name)
       r.update(val: val)
     else
-      r = AppConfig.new(group: key, val: val)
+      r = AppConfig.new(group: group, name: name, val: val)
       r.save
     end
   end
