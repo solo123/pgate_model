5a4f53d726f9ce3ebe52774310490d2dd3ad7ccf
jimmy <solo123@21cn.com>
2016-11-06 10:47:25 +0800

    model

--- app/models/biz/pooul_api.rb
+++ app/models/biz/pooul_api.rb
@@ -59,5 +59,20 @@ module Biz
       c.save!
     end
 
+    def self.get_mab(js)
+      mab = []
+      js.keys.sort.each do |k|
+        mab << "#{k}=#{js[k].to_s}" if k != :mac && k != :sign && js[k]
+      end
+      mab.join('&')
+    end
+    def self.md5(str)
+      Digest::MD5.hexdigest(str)
+    end
+    def self.get_mac(js, key)
+      md5(get_mab(js) + key).upcase
+    end
+
+
   end
 end
