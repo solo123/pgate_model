e3cf570409911996d1ec18271e20d405ad425d8a
jimmy <solo123@21cn.com>
2016-12-14 23:02:52 +0800

    agent

---
+++ app/models/agent.rb
@@ -0,0 +1,7 @@
+class Agent < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  belongs_to :org
+end
