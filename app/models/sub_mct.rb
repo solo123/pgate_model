109bd6aed953779d996a65dbf18e7fd28202d5cf
jimmy <solo123@21cn.com>
2016-12-27 16:06:59 +0800

    sub mct

--- app/models/sub_mct.rb
+++ app/models/sub_mct.rb
@@ -2,7 +2,19 @@ class SubMct < ApplicationRecord
   belongs_to :org
   belongs_to :bank_mct, polymorphic: true
   def clearing_type_enum
-    {'T1': 1, 'D0': 2}
+    {'待定': 0, 'T1': 1, 'D0': 2}
+  end
+
+  def bank_mct_type_enum
+    {"中信直连": 'ZxMct', '商联：合众易宝': 'hzyb'}
+  end
+
+  def pay_channel_type_enum
+    {"微信": 0, "支付宝": 1}
+  end
+
+  def status_enum
+    {"新商户(未发送)": 0, "已进件": 1, "有效商户": 8, "无效商户": 7}
   end
 
 end
